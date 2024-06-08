8efb7d3543d9bf0603f8572aeb9463f628c38357
flatw <argat2480@gmail.com>
2024-06-08 13:01:24 +0600

    reafactoring

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,14 +1,28 @@
 import styles from "./styles.module.css";
 
 export const Categories = ({categories, setSelectedCategory, selectedCategory}) => {
-    return <div className={styles.categories}>
-        {categories.map(category=>{
-            return <button 
-                className={selectedCategory === category ? styles.active : styles.item} 
-                key={category}
-                onClick={()=>setSelectedCategory(category)}>
-                {category}
+    return (
+      <div className={styles.categories}>
+        <button
+          className={!selectedCategory ? styles.active : styles.item}
+          onClick={() => setSelectedCategory(null)}
+        >
+          All
+        </button>
+
+        {categories.map((category) => {
+          return (
+            <button
+              className={
+                selectedCategory === category ? styles.active : styles.item
+              }
+              key={category}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
             </button>
+          );
         })}
-    </div>
+      </div>
+    );
 }
